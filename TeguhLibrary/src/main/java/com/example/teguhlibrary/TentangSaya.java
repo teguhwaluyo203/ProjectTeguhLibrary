cbd94535c90c7aa58a45ffecfc20cec2938d7641
teguhwaluyo203 <waluyoteguh795@gmail.com>
2021-01-07 19:26:23 +0700

    koneksi git

---
+++ TeguhLibrary/src/main/java/com/example/teguhlibrary/TentangSaya.java
@@ -0,0 +1,14 @@
+package com.example.teguhlibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TentangSaya extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang_saya);
+    }
+}
